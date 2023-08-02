d775f827be096cc1520f311a6fbc289c565af2b8
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-08-02 13:49:06 -0400

    Lab Completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,16 @@
 def oxford_comma(items):
-    return None
+    if len(items)==2:
+        new_string=" and ".join(items);
+        return new_string;
+    elif len(items)==3:
+        new_string=", ".join(items[0:2])
+        new_string=new_string+", and "+ items[2]
+        #new_string=new_string + " and " + items[3]
+        return new_string
+    elif len(items)>3:
+        new_string=", ".join(items[0:-1])
+        new_string=new_string+", and "+ items[-1]
+        return new_string
+    else:
+        new_string=", ".join(items);
+        return new_string;
